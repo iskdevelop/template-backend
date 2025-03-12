ef70ed5073ff4c6ad2d4f4de9d37289a27bd6f8a
Kid A <thisismeamir@outlook.com>
2025-03-12 19:30:15 +0330

    2025 Gradle, build system and source folder structure

---
+++ src/test/java/com/iskportal/template_backend/TemplateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskportal.template_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
